8418fa87011a545c2efd885b4ef0f898c42f4670
stromjust <justas.stromilas@gmail.com>
2020-01-09 06:02:00 +0200

    Added parameters checks. Expanded linear parameter bounds due to small errors

--- voidcolor.js
+++ voidcolor.js
@@ -3,63 +3,63 @@
 class Voidcolor {
   constructor() {
     // ** Conversions to different color codings * /
-    this.lmsFiltered =    [[0.31399022, 0.63951294, 0.04649755],
+    this.lmsFilter =    [[0.31399022, 0.63951294, 0.04649755],
             		          [0.15537241, 0.75789446, 0.08670142],
             		          [0.01775239, 0.10944209, 0.87256922]];
-    this.rgbFiltered =    [[ 5.47221206, -4.6419601,   0.16963708],
+    this.rgbFilter =    [[ 5.47221206, -4.6419601,   0.16963708],
                           [-1.1252419,   2.29317094, -0.1678952],
                           [ 0.02980165, -0.19318073,  1.16364789]];
 
     // ** Static filters * /
-    this.protanopia =    [[0, 1.05118294, -0.05116099],
-                          [0, 1,           0],
-                          [0, 0,           1]];
-    this.deuteranopia =  [[1,         0, 0],
-                          [0.9513092, 0, 0.04866992],
-                          [0,         0, 1]];
-    this.tritanopia =    [[ 1,          0,          0],
-       					          [ 0,          1,          0],
-                          [-0.86744736, 1.86727089, 0]];
+    this.protanopiaFilter =   [[0, 1.05118294, -0.05116099],
+                               [0, 1,           0],
+                               [0, 0,           1]];
+    this.deuteranopiaFilter = [[1,         0, 0],
+                               [0.9513092, 0, 0.04866992],
+                               [0,         0, 1]];
+    this.tritanopiaFilter =   [[ 1,          0,          0],
+       					               [ 0,          1,          0],
+                               [-0.86744736, 1.86727089, 0]];
   }
 
-  // ** Static filters * /
+  // ** Static functions * /
 
-  protanopiaFilter(r, g, b) {
-  	return this.transform({r, g, b}, this.protanopia);
+  protanopia(r, g, b) {
+  	return this.transform({r, g, b}, this.protanopiaFilter);
   }
 
-  deuteranopiaFilter(r, g, b) {
-    return this.transform({r, g, b}, this.deuteranopia);
+  deuteranopia(r, g, b) {
+    return this.transform({r, g, b}, this.deuteranopiaFilter);
   }
 
-  tritanopiaFilter(columnVector) {
-    return this.transform({r, g, b}, this.tritanopia);
+  tritanopia(r, g, b) {
+    return this.transform({r, g, b}, this.tritanopiaFilter);
   }
 
-  // ** Dynamic filters * /
+  // ** Dynamic functions * /
   //
   // dynamic filters are defined each time they're called,
-  // due to having better tested performance while being being async safe
+  // due to having better tested performance while being being thread/async safe
 
-  protanomalyFilter(r, g, b, ratio) {
+  protanomaly(r, g, b, ratio) {
     const protanomaly = [[1 - (1*ratio), 1.05118294 * ratio, -0.05116099 * ratio],
                          [0,             1,                   0],
                          [0,             0,                   1]];
-    return this.transform({r, g, b}, filter);
+    return this.transform({r, g, b}, protanomaly);
   }
 
-  deuteranomalyFilter(r, g, b, ratio) {
+  deuteranomaly(r, g, b, ratio) {
     const deuteranomaly = [[1,                 0,             0],
                            [0.9513092 * ratio, 1 - (1*ratio), 0.04866992 * ratio],
                            [0,                 0,             1]];
-    return this.transform({r, g, b}, filter);
+    return this.transform({r, g, b}, deuteranomaly);
   }
 
-  tritanomalyFilter(r, g, b, ratio) {
+  tritanomaly(r, g, b, ratio) {
     const tritanomaly = [[ 1,                  0,                  0],
 	                       [ 0,                  1,                  0],
                          [-0.86744736 * ratio, 1.86727089 * ratio, 1 - (1*ratio)]];
-    return this.transform({r, g, b}, filter);
+    return this.transform({r, g, b}, tritanomaly);
   }
 
   /**
@@ -74,9 +74,9 @@ class Voidcolor {
   transform(color, filter) {
     let vector = this.toColumnVector(color);
     let rgb = this.standardToLinearRGB(vector);
-    let lms = this.dot(this.lmsFiltered, rgb);
+    let lms = this.dot(this.lmsFilter, rgb);
     let lmsFiltered = this.dot(filter, lms);
-    let rgbFiltered = this.dot(this.rgbFiltered, lmsFiltered);
+    let rgbFiltered = this.dot(this.rgbFilter, lmsFiltered);
     vector = this.linearToStandardRGB(rgbFiltered);
     return { r: vector[0][0], g: vector[1][0], b: vector[2][0] }
   }
@@ -89,14 +89,16 @@ class Voidcolor {
    * @return {array} Dot product of two-dimensional arrays / matrices A and B.
    */
   dot(A, B) {
-    const An = A.length, Am = A[0].length;
-    const Bn = B.length, Bm = B[0].length;
-    let matrix = new Array(Am);
-    for(var r = 0; r < An; r++) {
-      matrix[r] = new Array(Bm);
-      for(var c = 0; c < Bm; c++) {
+    if(!Array.isArray(A) || !Array.isArray(A)) throw new Error('expected 2d arrays');
+    const Am = A.length, An = A[0].length;
+    const Bm = B.length, Bn = B[0].length;
+    if(An != Bm) throw new Error(`dimensions incompatible An:${An}, Bm:${Bm}`);
+    let matrix = new Array(An);
+    for(let r = 0; r < Am; r++) {
+      matrix[r] = new Array(Bn);
+      for(let c = 0; c < Bn; c++) {
         matrix[r][c] = 0;
-        for(var i = 0; i < Am; i++) {
+        for(let i = 0; i < An; i++) {
             matrix[r][c] += A[r][i] * B[i][c];
         }
       }
@@ -168,6 +170,10 @@ class Voidcolor {
    * Transformation of linear RGB channel 0.0-1.0 to non-linear
    * standard RGB channel 0-255
    *
+   * Note: After transformations, some times small error is introduced
+   *       and some linear values end up slightly out of bounds, which is normal
+   *       hence more lenient bound checks
+   *
    * https://en.wikipedia.org/wiki/SRGB#Specification_of_the_transformation
    * @param {float} linear - linear representation of sRGB, float ranging 0.0-1.0
    * @return {int} a single sRGB channel, integer ranging 0-255
@@ -175,15 +181,14 @@ class Voidcolor {
   linearToStandardChannel(linear) {
     if(typeof linear === 'string' ||
        typeof linear === 'undefined') throw new Error('float expected');
-    if(linear < 0 ||
-       linear > 1) throw new Error('parameter much be within range [0.0 - 1.0]');
+    if(linear < -0.15 ||
+       linear > 1.15) throw new Error(`parameter must be within range [0.0 - 1.0]: ${linear}`);
     let standard;
     if(linear <= 0.0031308) standard = linear * 12.92;
     else standard = 1.055 * Math.pow(linear, 1.0 / 2.4) - 0.055;
     return Math.round(standard * 255);
   }
 }
-
 
 module.id = 'voidcolor';
 module.exports = new Voidcolor();
